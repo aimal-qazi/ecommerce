3e3e5af91a3f8c78146340314faa26279b13200c
aimal-qazi <aimalqazi32@gmail.com>
2025-03-02 11:41:51 +0500

    add products controller

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,12 @@
-import express from 'express';
+import express, { json, urlencoded } from 'express';
+import productsRouter from './routes/products/products';
 
 const app = express();
 const port = 5000;
+
+app.use(urlencoded({ extended: true }));
+app.use(json());
+
+app.use('/products', productsRouter);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
